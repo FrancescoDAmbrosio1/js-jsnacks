0c7ed33865ad34e0e273a1a016ca372f147b7e80
FrancescoDAmbrosio1 <ciccio.dambrosio@hotmail.it>
2024-03-20 01:53:53 +0100

    ADD: jsnack2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,46 @@
 //                          JSNACK 1
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
-//PROCEDIMENTO JSNACK1
+//      PROCEDIMENTO JSNACK1
 //chiedere all'utente il primo numero num1
 //chiedere all'utente il seconodo numero num2
 //SE num1>num2 allora stampo num1
 //ALTRIMENTI SE num1=num2 allora stampo che i numeri sono uguali
 //ALTRIMENTI stampo num2 
 
-let num1 = parseInt(prompt('Inserisci il primo numero'));     //number
-let num2 = parseInt(prompt('Inserisci il secondo numero'))    //number
-if(num1 > num2){
-    console.log('Il numero maggiore è: ', num1);    //string
-} else if(num1 < num2){
-    console.log('I numeri maggiore è', num2);   //string
-} else {
-    console.log('I numeri inseriti sono uguali');    //string
-}
+// let num1 = parseInt(prompt('Inserisci il primo numero'));     //number
+// let num2 = parseInt(prompt('Inserisci il secondo numero'))    //number
+// if(num1 > num2){
+//     console.log('Il numero maggiore è: ', num1);    //string
+// } else if(num1 < num2){
+//     console.log('I numeri maggiore è', num2);   //string
+// } else {
+//     console.log('I numeri inseriti sono uguali');    //string
+// }
+
+
+
+//                          JSNACK 2
+//L’utente inserisce due parole in successione, con due prompt. 
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+//      PROCEDIMENTO JSNACK2
+//chiedere all'utente con il prompt la prima parola
+//chiedere all'utente con il prompt la seconda parola
+//calcolare la lunghezza della prima parola
+//calcolare la lunghezza della seconda parola
+
+// const primaParola = prompt('Inserisci la prima parola');    //string
+// const secondaParola = prompt('Inserisci la seconda parola') //string
+// const lunghezzaPrimaParola = primaParola.length //number
+// const lunghezzaSecondaParola = secondaParola.length //number
+// if(lunghezzaPrimaParola > lunghezzaSecondaParola){
+//     console.log(primaParola);
+//     console.log(secondaParola);
+// } else {
+//     console.log(secondaParola);
+//     console.log(primaParola);
+// }
+
+
+
+
+//                          JSNACK 3
